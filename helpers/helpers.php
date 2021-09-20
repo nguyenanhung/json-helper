20d19cd02afe6118af88380c74c7744da1f538d7
nguyenanhung <info@nguyenanhung.com>
2021-09-21 00:58:10 +0700

    Add helper

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -7,3 +7,36 @@
  * Date: 09/21/2021
  * Time: 00:51
  */
+if (!function_exists('jsonItem')) {
+    /**
+     * Function jsonItem
+     *
+     * @param string $json_string
+     * @param string $item_output
+     *
+     * @return string|null
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 54:52
+     */
+    function jsonItem($json_string = '', $item_output = '')
+    {
+        return nguyenanhung\Libraries\JSON\Json::jsonItem($json_string, $item_output);
+    }
+}
+if (!function_exists('isJson')) {
+    /**
+     * Function isJson
+     *
+     * @param string $string
+     *
+     * @return bool
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 08/18/2021 54:50
+     */
+    function isJson($string = '')
+    {
+        return nguyenanhung\Libraries\JSON\Json::isJson($string);
+    }
+}
