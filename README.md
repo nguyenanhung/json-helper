0277982db7575319419752ca614711fba35e2811
nguyenanhung <info@nguyenanhung.com>
2021-09-21 00:55:49 +0700

    Create Project

---
+++ README.md
@@ -0,0 +1,15 @@
+[![Latest Stable Version](http://poser.pugx.org/nguyenanhung/escape-helper/v)](https://packagist.org/packages/nguyenanhung/escape-helper) [![Total Downloads](http://poser.pugx.org/nguyenanhung/escape-helper/downloads)](https://packagist.org/packages/nguyenanhung/escape-helper) [![Latest Unstable Version](http://poser.pugx.org/nguyenanhung/escape-helper/v/unstable)](https://packagist.org/packages/nguyenanhung/escape-helper) [![License](http://poser.pugx.org/nguyenanhung/escape-helper/license)](https://packagist.org/packages/nguyenanhung/escape-helper) [![PHP Version Require](http://poser.pugx.org/nguyenanhung/escape-helper/require/php)](https://packagist.org/packages/nguyenanhung/escape-helper)
+
+# JSON Helper
+
+Basic and Advanced JSON Helper
+
+## Contact & Support
+
+If any question & request, please contact following information
+
+| Name        | Email                | Skype            | Facebook      |
+| ----------- | -------------------- | ---------------- | ------------- |
+| Hung Nguyen | dev@nguyenanhung.com | nguyenanhung5891 | @nguyenanhung |
+
+From Vietnam with Love <3
---
+++ src/Json.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Project json-helper
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/21/2021
+ * Time: 00:52
+ */
+
+namespace nguyenanhung\Libraries\JSON;
+
+use Laminas\Json\Json as BaseJson;
+
+/**
+ * Class Json
+ *
+ * @package   nguyenanhung\Libraries\JSON
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+class Json extends BaseJson
+{
+    /**
+     * Function jsonItem
+     *
+     * @param string $json_string
+     * @param string $item_output
+     *
+     * @return null|string
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 9/29/18 11:08
+     *
+     */
+    public static function jsonItem($json_string = '', $item_output = '')
+    {
+        $result      = json_decode(trim($json_string), true);
+        $item_output = trim($item_output);
+        if (($result !== null) && isset($result[$item_output])) {
+            return trim($result[$item_output]);
+        }
+
+        return null;
+    }
+
+    /**
+     * Function isJson
+     *
+     * @param string $json
+     *
+     * @return bool TRUE or FALSE
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 10/13/18 09:39
+     *
+     */
+    public static function isJson($json = '')
+    {
+        $decode = json_decode(trim($json), true);
+
+        return !($decode === null);
+    }
+}
